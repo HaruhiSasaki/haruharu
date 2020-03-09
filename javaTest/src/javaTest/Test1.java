4d8cf42c05ebfd289f44079a6c8babe29bf33844
haruhi sasaki <onedraft82@yahoo.co.jp>
2020-03-09 16:10:23 +0900

    付録１記載

--- javaTest/src/javaTest/Test1.java
+++ javaTest/src/javaTest/Test1.java
@@ -4,7 +4,12 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		System.out.println(c);
+		c = c + 1;
+		System.out.println(c);
 	}
 
 }
